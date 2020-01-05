145006ab31105166579de4c9b5906a85fd0ff59f
Nikola123-coder <57827830+Nikola123-coder@users.noreply.github.com>
2020-01-05 17:01:24 +0100

    Add files via upload

---
+++ Zad_6.py
@@ -0,0 +1,18 @@
+import turtle
+
+bogdan = turtle.Turtle()
+
+
+
+bogdan.forward(100)
+bogdan.right(90)
+bogdan.forward(100)
+bogdan.right(90)
+bogdan.forward(100)
+bogdan.right(90)
+bogdan.forward(100)
+bogdan.forward(100)
+
+
+
+turtle.done()
